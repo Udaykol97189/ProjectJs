59920197c6bb712d1dfc87969de21a6fd2dd8a3b
uday <koludaykol@gmail.com>
2022-07-01 21:22:42 +0530

    two

--- App.js
+++ App.js
@@ -7,8 +7,13 @@ export default class MyApp extends React.Component {
   }
   render() {
     return (
-      <View>
-        <Text>Hallo</Text>
+      <View style={{width:"100%",height:"100%"}}>
+        <View style={{width:"100%",justifyContent:'center',height:50,backgroundColor:'black'}}>
+         <Text style={{color:'white',marginLeft:10,fontSize:16}}>App Bar</Text>
+        </View>
+        <View style={{width: "100%",height: "100%",alignItems:'center',justifyContent:'center'}}>
+          <Text style={{ color: 'black',fontWeight:'bold',fontSize: 16 }}>Hallo Uday</Text>
+        </View>
       </View>
     );
   }
